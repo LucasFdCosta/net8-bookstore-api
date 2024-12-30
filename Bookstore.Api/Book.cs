25a7263ca558e5d3674b5e0a68aadb8862778529
Lucas Costa <lucas.co090869@gmail.com>
2024-12-30 15:29:18 -0300

    feat: Add books endpoints
    Add get, get by id, create, update and delete endpoints

---
+++ Bookstore.Api/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bookstore.Api
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public BookGenre Genre { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public enum BookGenre
+    {
+        Fiction,
+        Romance,
+        Mystery,
+        Economy,
+        Politics,
+        Philosophy
+    }
+}
--- Bookstore.Api/Controllers/BooksController.cs
+++ Bookstore.Api/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Bookstore.Api.Controllers;
 
@@ -6,4 +7,98 @@ namespace Bookstore.Api.Controllers;
 [ApiController]
 public class BooksController : ControllerBase
 {
+    [HttpGet("{id:int:min(1)}", Name = "GetById")]
+    [ProducesResponseType(typeof(Book), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    public IActionResult GetById(int id)
+    {
+        var book = new Book()
+        {
+            Id = 1,
+            Author = "Lucas",
+            Genre = BookGenre.Mystery,
+            Quantity = 10,
+            Title = "A Really mysterious book",
+            Price = 10.5
+        };
+
+        if (id == 1) return Ok(book);
+
+        return NotFound("No book was found with this ID!");
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+    public IActionResult Get()
+    {
+        var books = new List<Book>()
+        {
+            new Book
+            {
+                Id = 1,
+                Author = "Lucas",
+                Genre = BookGenre.Mystery,
+                Quantity = 10,
+                Title = "A Really mysterious book",
+                Price = 10.5
+            },
+            new Book
+            {
+                Id = 2,
+                Author = "John Doe",
+                Genre = BookGenre.Philosophy,
+                Quantity = 7,
+                Title = "What should I do?",
+                Price = 15.5
+            },
+            new Book
+            {
+                Id = 3,
+                Author = "Bob",
+                Genre = BookGenre.Fiction,
+                Quantity = 5,
+                Title = "Bob's adventure",
+                Price = 20
+            },
+            new Book
+            {
+                Id = 4,
+                Author = "Alice",
+                Genre = BookGenre.Romance,
+                Quantity = 6,
+                Title = "A Really romantic book",
+                Price = 25.9
+            },
+        };
+
+        return Ok(books);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Book), StatusCodes.Status201Created)]
+    public IActionResult Post([FromBody] Book book)
+    {
+        return Created(string.Empty, book);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(typeof(Book), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    public IActionResult Put([FromRoute] int id, [FromBody] Book book)
+    {
+        if (id == 1) return Ok(book);
+
+        return NotFound("No book was found with this ID!");
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(typeof(Book), StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    public IActionResult Delete(int id)
+    {
+        if (id == 1) return NoContent();
+
+        return NotFound("No book was found with this ID!");
+    }
 }
